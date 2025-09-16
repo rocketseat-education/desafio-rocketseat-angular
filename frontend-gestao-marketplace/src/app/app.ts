8604f33c92c3633fc0e5af058c7d4d3aaa376590
feelipefreitas <feelipe.freitas97@gmail.com>
2025-09-16 20:07:39 -0300

    Adicionando estilos da página de produtos

--- frontend-gestao-marketplace/src/app/app.ts
+++ frontend-gestao-marketplace/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Login } from "./pages/login/login";
 import { Header } from "./pages/header/header";
+import { Products } from "./pages/products/products";
 
 @Component({
   selector: 'app-root',
-  imports: [Login, Header],
+  imports: [Login, Header, Products],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
