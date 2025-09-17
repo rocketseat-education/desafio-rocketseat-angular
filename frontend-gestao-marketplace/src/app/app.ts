6a62c7bea3d2660de5ebf85f08aff6e20e1fa663
feelipefreitas <feelipe.freitas97@gmail.com>
2025-09-17 10:12:29 -0300

    Adicionando estilos da página de editar produto

--- frontend-gestao-marketplace/src/app/app.ts
+++ frontend-gestao-marketplace/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Login } from "./pages/login/login";
-import { Header } from "./pages/header/header";
+import { Header } from "./components/header/header";
 import { Products } from "./pages/products/products";
+import { EditProduct } from "./pages/edit-product/edit-product";
 
 @Component({
   selector: 'app-root',
-  imports: [Login, Header, Products],
+  imports: [Login, Header, Products, EditProduct],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
