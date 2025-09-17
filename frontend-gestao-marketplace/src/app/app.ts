00c029d272cdde43fb47103375842651b2cfa415
feelipefreitas <feelipe.freitas97@gmail.com>
2025-09-17 10:46:35 -0300

    Adicionando estilos da página de criar produto

--- frontend-gestao-marketplace/src/app/app.ts
+++ frontend-gestao-marketplace/src/app/app.ts
@@ -3,10 +3,11 @@ import { Login } from "./pages/login/login";
 import { Header } from "./components/header/header";
 import { Products } from "./pages/products/products";
 import { EditProduct } from "./pages/edit-product/edit-product";
+import { NewProduct } from "./pages/new-product/new-product";
 
 @Component({
   selector: 'app-root',
-  imports: [Login, Header, Products, EditProduct],
+  imports: [Login, Header, Products, EditProduct, NewProduct],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
