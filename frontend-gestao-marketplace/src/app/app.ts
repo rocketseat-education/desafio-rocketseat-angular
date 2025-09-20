7c326384b2ca41d4c0a6f63ac3cf76c978f1c3a9
feelipefreitas <feelipe.freitas97@gmail.com>
2025-09-20 06:51:19 -0300

    Remoção do componente de edição

--- frontend-gestao-marketplace/src/app/app.ts
+++ frontend-gestao-marketplace/src/app/app.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { Login } from "./pages/login/login";
 import { Header } from "./components/header/header";
 import { Products } from "./pages/products/products";
-import { EditProduct } from "./pages/edit-product/edit-product";
 import { NewProduct } from "./pages/new-product/new-product";
 
 @Component({
   selector: 'app-root',
-  imports: [Login, Header, Products, EditProduct, NewProduct],
+  imports: [Login, Header, Products, NewProduct],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
