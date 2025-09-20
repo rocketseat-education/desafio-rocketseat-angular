376e4c13923649d23bf1c9bf0e8d17bb894d1573
feelipefreitas <feelipe.freitas97@gmail.com>
2025-09-20 07:25:15 -0300

    Aumentando limite de upload

--- backend-gestao-marketplace/src/server.ts
+++ backend-gestao-marketplace/src/server.ts
@@ -14,8 +14,8 @@ const PORT = process.env.PORT || 3000;
 
 // Middlewares globais
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 
 // Middleware de log para desenvolvimento
 app.use((req, res, next) => {
